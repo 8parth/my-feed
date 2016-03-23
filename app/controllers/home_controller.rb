2c57f82352331d98b1a37dd0a79a7dcbdc4e8252
8parth <parthmodi54@yahoo.com>
2016-03-23 13:15:10 +0530

    gotham added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,18 +1,27 @@
 class HomeController < ApplicationController
   def index
-  	url = "http://showrss.info/feeds/505.rss"
-  	feed =  Feedjira::Feed.fetch_and_parse(url)
-  	@new_feed = Feed.create(title: feed.title, url: feed.url)
-  	puts @new_feed.inspect  
+    url = "http://showrss.info/feeds/505.rss"
+    #url2 = "http://showrss.info/feeds/928.rss"
+    feed =  Feedjira::Feed.fetch_and_parse(url)
+    @new_feed = Feed.create(title: feed.title, url: feed.url)
+    puts @new_feed.inspect  
 
 
-  	@entries = feed.entries
+    @entries = feed.entries
 
-  	puts @entries.inspect
-
-  	
+    puts @entries.inspect
   end
 
-  def show
+  def gotham
+    url = "http://showrss.info/feeds/928.rss"
+    feed =  Feedjira::Feed.fetch_and_parse(url)
+    @new_feed = Feed.create(title: feed.title, url: feed.url)
+    puts @new_feed.inspect  
+
+
+    @entries = feed.entries
+
+    puts @entries.inspect
+
   end
 end
